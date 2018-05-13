ad501c6734fde0b7ebe943b85162fed258fc28dc
Lisa Chan <xlisachan@gmail.com>
2018-05-13 19:49:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+row = 1.times { put "   |   |  " }
+divider = 11.times { print "-" }
+
+def display_board
+  puts row + /n
+  puts divider + /n
+  puts row + /n
+  puts divider + /n
+  puts row + /n
+end
+
+display_board
